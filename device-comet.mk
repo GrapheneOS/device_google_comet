18acbae46e1a8ecdbd2dd9018aadb8d0b2805b3e
George <georgekgchang@google.com>
2023-04-11 10:39:05 +0800

    [eSE] Update AIDL secure_element for eSE1
    Bug: 261566299
    Test: run cts -m CtsOmapiTestCases
    Test: atest VtsAidlHalNfcTargetTest
    Merged-In: I990e2be2cf9750f0e041dd1f571eb33613a1ce6f
    Change-Id: I990e2be2cf9750f0e041dd1f571eb33613a1ce6f

--- device-comet.mk
+++ device-comet.mk
@@ -64,7 +64,7 @@ PRODUCT_PACKAGES += \
 
 # SecureElement
 PRODUCT_PACKAGES += \
-	android.hardware.secure_element@1.2-service-gto
+	android.hardware.secure_element-service.thales
 
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
