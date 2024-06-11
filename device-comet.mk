c5ab6329ccea24fdcd8b033d06da8626bfec42b5
Daniel Micay <daniel.micay@grapheneos.org>
2024-06-11 14:33:23 -0400

    remove ro.vendor.build.svn sysprop
    Value of ro.vendor.build.svn in stock OS image used by adevtool might not match the value specified
    here, e.g. when base AOSP tag doesn't match the tag that was used for stock OS build.
    
    adevtool automatically adds missing properties, i.e. removal of ro.vendor.build.svn from here means
    that ro.vendor.build.svn sysprop value from stock OS image will be used instead.

--- device-comet.mk
+++ device-comet.mk
@@ -546,3 +546,5 @@ PRODUCT_COPY_FILES += \
 # sysconfigs from stock OS
 PRODUCT_COPY_FILES += \
     device/google/comet/product-sysconfig-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/product-sysconfig-stock.xml
+
+PRODUCT_VENDOR_PROPERTIES := $(filter-out ro.vendor.build.svn=% , $(PRODUCT_VENDOR_PROPERTIES))
