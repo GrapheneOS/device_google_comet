e227bcf57ad5b3928d2fd52954d763a0e7c5db74
Jimi Chen <jimichen@google.com>
2024-10-15 20:42:10 +0800

    add fatp_camera_eeprom_inspector for comet
    Bug: 356518766
    Test: adb shell vendor/bin/camera_eeprom_inspector
    Flag: NONE new tool
    Change-Id: Icc4a29c24f5e534ca6750df4b9251ba69e3b735e

--- factory_comet.mk
+++ factory_comet.mk
@@ -45,6 +45,7 @@ PRODUCT_PACKAGES += \
 
 # Factory binary of camera
 PRODUCT_PACKAGES += fatp_ct3_wide_hat_tool fatp_ct3_tele_hat_tool fatp_ct3_ultrawide_hat_tool
+PRODUCT_PACKAGES += fatp_camera_eeprom_inspector
 
 PRODUCT_WITHOUT_TTS_VOICE_PACKS := true
 
